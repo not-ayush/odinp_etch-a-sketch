e5b7e7d339bd4e45c49230816fc92a3955665c30
slowbeeps <gangwarayush921@gmail.com>
2020-08-05 19:09:56 +0530

    added randomColor & grayScale

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const container = document.getElementById('container');
 const resetBtn = document.getElementById('reset');
-const clearGridBtn = document.getElementById('clear')
+const clearGridBtn = document.getElementById('clear');
+const randomClrBtn = document.getElementById('random');
+const grayscaleBtn = document.getElementById('gray');
+const defaultBtn = document.getElementById('default');
+
 let sideSize = 16;
-let gridTotalCells = sideSize**2;
+let gridTotalCells = sideSize ** 2;
+
+function clearGrid() {
+    let cellDivsArr = document.querySelectorAll('#container div')
+    cellDivsArr.forEach((node) => {
+        node.style.backgroundColor = "rgb(0,0,0,0)";
+        node.style.opacity = "1";
+        node.style = "";
+    })
+}
 
 function makeGrid() {
     for (let i = 0; i <= gridTotalCells; i++) {
@@ -10,12 +23,8 @@ function makeGrid() {
         newDivCell.setAttribute('id', `${i}`);
         container.appendChild(newDivCell);
         newDivCell.addEventListener("mouseover", (e) => {
-            // to prevent needless adding hovered:
-            if ((e.target.classList)[0] == 'hovered') {
-                return;
-            } else {
-                newDivCell.classList.add("hovered");
-            }
+            if (newDivCell.style.backgroundColor == "black") return;
+            else newDivCell.style.backgroundColor = "black";
         })
     }
     container.style.cssText = `display: grid; grid-template-columns: repeat(${sideSize}, 1fr); grid-template-rows: repeat(${sideSize}, 1fr);`;
@@ -26,16 +35,61 @@ makeGrid();
 resetBtn.addEventListener('click', () => {
     container.innerHTML = "";
     sideSize = prompt("What size do you want it to be?");
-    gridTotalCells = sideSize**2;
+    if (sideSize > 150) {
+        sideSize = prompt("It will take forever, enter something lower than 150!");
+    } else if (isNaN(sideSize)) sideSize = 16;
+    gridTotalCells = sideSize ** 2;
     makeGrid();
 })
 
 clearGridBtn.addEventListener('click', () => {
+    clearGrid();
+})
+
+defaultBtn.addEventListener('click', () => {
+    container.innerHTML = "";
+    makeGrid();
     let cellDivsArr = document.querySelectorAll('#container > div')
     cellDivsArr.forEach((node) => {
-        node.classList.remove('hovered');
+        node.addEventListener('mouseover', () => {
+            if (node.style.backgroundColor == "black") {
+                return;
+            } else node.style.backgroundColor = "black";
+        })
     })
 })
+
+randomClrBtn.addEventListener('click', () => {
+    container.innerHTML = "";
+    makeGrid();
+    let cellDivsArr = document.querySelectorAll('#container > div')
+    cellDivsArr.forEach((node) => {
+        node.addEventListener('mouseover', () => {
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            node.style.backgroundColor = `rgb(${r},${g},${b})`;
+        })
+    })
+})
+
+grayscaleBtn.addEventListener('click', () => {
+    container.innerHTML = "";
+    makeGrid();
+    let cellDivsArr = document.querySelectorAll('#container > div');
+    cellDivsArr.forEach((node) => {
+        node.style.backgroundColor = "black";
+        node.style.opacity = "0.0";
+        node.addEventListener('mouseover', (e) => {
+            // node.style.backgroundColor = "black";
+            // node.style.opacity = "0.0";
+            let currentOpacity = e.target.style.opacity;
+            node.style.opacity = Number(currentOpacity) + 0.1;
+        })
+    })
+})
+
+
 
 // My "plan":
 /* Goal_1: create a 16x16 grid with divs using JS only.
@@ -61,6 +115,3 @@ Goal_3: reset grid size button
         c. square that row size and store it in the gridTotalCells variable.
         d. then make the grid again.
     - I didn't recalculate gridTotalSize when reset button was clicked, that led to only 256 divs being created, but it's fixed now.  */
-
-
-
