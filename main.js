d2cf88421f7133c64fdbf0bd5e81d41c39c354e5
slowbeeps <gangwarayush921@gmail.com>
2020-08-05 13:10:42 +0530

    completed goal_2 - hover effect on cells.

--- main.js
+++ main.js
@@ -7,14 +7,26 @@ Goal_1: create a 16x16 grid with divs using JS only.
         b. grid template-coloumn repaet(16, 1fr)
         c. grid template-row repaet(16, 1fr)
         This will be done by div.cssText.
+
+Goal_2: hover effect to change color of cells
+    i. add a mouseover eventlistener which changes cell.css.background-color to black.
+    ii. then remove the event listener in the end so that color isn't changed on every mouse over. So, I just ended up using conditionals to prevent unessecary execution because it was simpler this way.
 */
 
 const container = document.getElementById('container');
 
-for (let i = 0; i <= 216; i++) {
+// Display 16 x 16 grid:
+for (let i = 0; i <= 256; i++) {
     let newDivCell = document.createElement('div');
     newDivCell.setAttribute('id', `${i}`);
     container.appendChild(newDivCell);
+    // add hovered class if not added already, which changes bgc to black:
+    newDivCell.addEventListener("mouseover", (e) => {
+        if ((e.target.classList)[0] == 'hovered') {
+            return;
+        } else {
+            newDivCell.classList.add("hovered");
+        }
+    })
 }
-
 container.style.cssText = "display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);";
